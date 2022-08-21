e6845abc00b1231d5c17b357c1d561cbc3e92123
eunjungL <dldmswjd322@gmail.com>
2022-08-21 16:58:57 +0900

    first commit

---
+++ src/main/java/com/example/kurly_hack_festa/KurlyHackFestaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kurly_hack_festa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurlyHackFestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurlyHackFestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kurly_hack_festa/KurlyHackFestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kurly_hack_festa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurlyHackFestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
