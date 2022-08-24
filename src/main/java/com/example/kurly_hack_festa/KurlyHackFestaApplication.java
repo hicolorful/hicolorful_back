b8e8e05ea46f2cbebd62fbcf30af0915a307dee3
eunjungL <dldmswjd322@gmail.com>
2022-08-24 15:12:54 +0900

    fix: timezone 서울로 지정

--- src/main/java/com/example/kurly_hack_festa/KurlyHackFestaApplication.java
+++ src/main/java/com/example/kurly_hack_festa/KurlyHackFestaApplication.java
@@ -6,8 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class KurlyHackFestaApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KurlyHackFestaApplication.class, args);
